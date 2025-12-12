289d84cda95a2d34f207608a1fe497f13114922b
juarezz29 <juarezzsheila@gmail.com>
2025-12-12 18:12:54 -0300

    commit reportes

--- test_login.py
+++ test_login.py
@@ -1,28 +1,25 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
- 
-driver = webdriver.Chrome()
-driver.implicitly_wait(5)
- 
-try:
-    #Login
-    driver.get("https://www.saucedemo.com/")
-    driver.find_element(By.ID,"user-name").send_keys("standard_user")
-    driver.find_element(By.ID,"password").send_keys("secret_sauce")
-    driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
- 
-    #Validación de la redirección de la página
-    assert '/inventory.html' in driver.current_url
- 
-    #Interacciones
-    productos = driver.find_elements(By.CLASS_NAME, "inventory_item")
-    productos[0].find_element(By.TAG_NAME, "button").click()  # Añadir al carrito el primer producto
-    carrito = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
-    assert carrito == '1'
- 
-    print("Test OK")
-   
-finally:
-    time.sleep(1)
-    driver.quit()
+
+def test_login():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+    try:
+        driver.get("https://www.saucedemo.com/")
+        driver.find_element(By.ID, "user-name").send_keys("standard_user")
+        driver.find_element(By.ID, "password").send_keys("secret_sauce")
+        driver.find_element(By.CSS_SELECTOR, "input[type='submit']").click()
+        
+        #Validación de la redirección de la página
+        assert '/inventory.html' in driver.current_url
+               
+         #Interacciones con la página de inventario
+        productos = driver.find_elements(By.CLASS_NAME, "inventory_item")
+        productos[0].find_element(By.TAG_NAME, "button").click()
+        carrito = driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+        assert carrito == '1'
+
+    finally:
+        time.sleep(2)
+        driver.quit()
